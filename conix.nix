8ff6c6a0fa0a8cab82b385c3185dbfe5348c8be7
theNerd247 <noah.harvey247 gm>
2020-08-12 00:44:32 -0400

    simplifying and refactoring conix.nix

--- conix.nix
+++ conix.nix
@@ -1,26 +1,38 @@
-let 
-  pkgs = import <nixpkgs> {};
-in with pkgs;
-rec 
+self: super:
+{ conix = (super.conix or {}) // rec
 {
-  fmap_ = g: f: x: g (f x);
+  mergeModule = 
+    # AttrSet -> AttrSet -> AttrSet
+    self.lib.attrsets.recursiveUpdate;
 
-  ap_ = g: f: x: g x (f x);
+  mergeModuleFunc 
+    # (AttrSet -> AttrSet) -> (AttrSet -> AttrSet) -> AttrSet -> AttrSet
+    = mkModuleA: mkModuleB: x: 
+    mergeModule (mkModuleA x) (mkModuleB x);
 
-  liftA2_ = h: g: f: ap_ (fmap_ h g) f;
+  # FoldMap for module functions 
+  foldMap 
+    # (a -> AttrSet -> AttrSet) -> [a] -> AttrSet -> AttrSet
+    = f: 
+      builtins.foldl' (m: x: mergeModuleFunc m (f x)) (x: {});
 
-  mergeModule = lib.attrsets.recursiveUpdate;
+  # Fold for module functions
+  fold
+    # [ (AttrSet -> AttrSet) ] -> AttrSet -> AttrSet
+    = foldMap (x: x);
 
-  pure_ = x: _: x;
+  # This is a convenience function so users don't clutter up their content
+  # with long bits of code for small things.
+  str = builtins.toString;
 
-  mergeModuleFunc = liftA2_ mergeModule;
+  # Constructor for creating storing text content.
+  text_ 
+    # (IsString t) => t -> { text : String }
+    = text: { text = str text; };
 
-  mergeMap_ = f: 
-    builtins.foldl' (m: x: mergeModuleFunc m (f x)) (pure_ {});
-
-  merge_ = mergeMap_ (x: x);
-
-  text_ = text: { text = s text; };
+  # A convenience function for creating 
+  # TODO: it might be a better user experience to rename this to `txt` instead.
+  t = text_;
 
   # This is the core function that makes conix work.  It merges the current
   # attribute set and preserves the concatenates the toplevel text values. If
@@ -37,7 +49,7 @@ rec
   #   (conix: { favorite = texts_ [ 
   #      "My " 
   #      ({ color = 256; text = "Blue"; })
-  #      " color is very ${s conix.favorite.color}"
+  #      " color is very ${str conix.favorite.color}"
   #   ];})
   #
   # will fail with an infinite recursion error. This is due purely because it
@@ -61,63 +73,60 @@ rec
   #     (conix: { favorite = texts_ [ 
   #        "My " 
   #        ({ color = conix.color.blue; text = "Blue"; })
-  #        " color is very ${s conix.color.blue}"
+  #        " color is very ${str conix.color.blue}"
   #     ];})
   #   ]
   # Will work.
   # 
   # 
   mergeTexts 
-    # forall r. { text : String | r } -> Lazy String | { text : String | r }  -> { text : String | r }
+    # forall r. { text : String | r } -> String | { text : String | r }  -> { text : String | r }
     = a: b:
-    if builtins.isString b 
-    then a // (text_ ((a.text or "") + b))
-    else (mergeModule a b) // (text_ ((a.text or "") + b.text)); 
+      if builtins.isString b 
+      then a // (text_ ((a.text or "") + b))
+      else (mergeModule a b) // (text_ ((a.text or "") + b.text)); 
 
-  texts_ = 
-    builtins.foldl' mergeTexts {};
+  # The toplevel user API function for creating text blocks that have labelled
+  # values inter-mixed. This allows users to content that can be referenced
+  # within other parts of their document.
+  # 
+  # See the documentation for mergeTexts.
+  texts_ 
+    # forall r. [  String | { text : String | r } ] -> AttrSet
+    = builtins.foldl' mergeTexts {};
 
-  # These are helper functions to make common functions less invasive.
-  # These may need to be thrown away later on.
-  s = builtins.toString;
+  # This is a convenience function so users don't have to write:
+  #  
+  #  conix: conix.fold [...] conix;
+  #
+  # (Remember: conix.fold [ ... ] : AttrSet -> AttrSet)
+  merge
+    # [ (AttrSet -> AttrSet) ] -> AttrSet
+    = x: fs: fold fs x;
 
-  t = text_;
+  mkLib = x: { lib =
+    { inherit 
+      str
+      t
+      texts_
+      foldMap
+      fold
+      mergeModule
+      mergeModuleFunc;
 
-  l = list: f:
-    texts_ (builtins.map f list);
+      merge = merge x;
 
-  conixApi = x: 
-    { inherit s t l texts_ merge_;
-      pkgs = pkgs;
+      pkgs = self;
     };
+  };
 
-  eval = mkModule: 
-    let
-      toplevel = mergeModuleFunc mkModule conixApi;
-    in
-      lib.fix toplevel;
-
-  test = merge_ 
-    [ 
-      (pure_ { c = 8; })
-      (x: { t = x.texts_ [
-        ''x.c = 
-            ${s x.c} +''(t x.t.x)'' - ''(t x.t.x)'' | t.x = ''({ x = 4; text = "4"; })''
-
-            =''(t (x.c + x.t.x))'' - ''(t x.t.x)''
-
-            = ${s x.c} 
-            qed.
-        ''];
-      })
-      (x: { color.blue = 256; })
-   
-      (conix: { favorite = conix.texts_ [ 
-         "My " 
-         ({ color = conix.color.blue; text = "Blue"; })
-         " color is very ${s conix.color.blue}"
-      ];})
-    ];
-
-  r = eval test;
-}
+  # This is the evaluator for the module functions. It's return value is the
+  # toplevel attribute set that contains all of the user's content.
+  eval 
+    # (AttrSet -> AttrSet) -> AttrSet
+    = mkModule: 
+      let
+        toplevel = mergeModuleFunc mkModule mkLib;
+      in
+        self.lib.fix toplevel;
+}; }
