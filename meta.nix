0001a3fb2d918269c7d7a6afc664b224ec96ba80
theNerd247 <noah.harvey247 gm>
2020-08-13 14:16:10 -0400

    updates meta.nix to use new api

--- meta.nix
+++ meta.nix
@@ -5,14 +5,25 @@ with super.conix;
 { conix = (super.conix or {}) //
   rec
   { 
-    homepageUrl = pureModule "https://github.com/theNerd247/conix.git";
-    homePageLink = pureModule "<a href=\"${homepageUrl.val}\">conix</a>";
-    buildStatusBadgeMd = pureModule "![CI](https://github.com/theNerd247/conix/workflows/CI/badge.svg?branch=master)";
+    homepageUrl = "https://github.com/theNerd247/conix.git";
+    gitHttpUrl = "https://github.com/theNerd247/conix.git"; 
+    homePageLink = "<a href=\"${homepageUrl.val}\">conix</a>";
+    buildStatusBadgeMd = "![CI](https://github.com/theNerd247/conix/workflows/CI/badge.svg?branch=master)";
     version = rec  
-      { text = pureModule "${builtins.toString major}.${builtins.toString minor}.${builtins.toString patch}";
+      { text = "${builtins.toString major}.${builtins.toString minor}.${builtins.toString patch}";
         major = 0; 
         minor = 1; 
         patch = 0; 
       };
+
+    lib = super.conix.extendLib super.conix.lib (x:
+      { inherit
+        homepageUrl
+        gitHttpUrl
+        homePageLink
+        buildStatusBadgeMd
+        version;
+      }
+    );
   };
 }
