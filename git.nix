0fb01b6e3c5593ddc06247e48025597b5a62b299
theNerd247 <noah.harvey247 gm>
2020-08-18 13:16:33 -0400

    updates git.nix

--- git.nix
+++ git.nix
@@ -2,7 +2,7 @@ conix: { lib = rec
   { git =
     rec
     { url = "https://github.com/theNerd247/conix.git"; 
-      rev = "30e750a6eb3acc5644a01c0d1759f19ac0e0e264";
+      rev = "10ddc35ad608ed67983a4713a845b785a143c3f1";
       ref = "v0.1.0-api";
       text = ''
         { url = "${url}";
