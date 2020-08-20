76df57b4f29a2a853a5836edef03acf12e27fbcc
theNerd247 <noah.harvey247 gm>
2020-08-20 17:54:48 -0400

    wip build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -ex
+
 # Pull down the branch originating the PR or branch pushed to
 # This makes it so we can create fixup commits
 if $TRAVIS_PULL_REQUEST; then
@@ -9,7 +11,7 @@ else
 fi
 
 git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
-git fetch $branch
+git fetch "$branch"
 git checkout -t origin/branch
 git config user.email "travis@travis.org"
 git config user.name "travis"
@@ -19,7 +21,7 @@ git config user.name "travis"
 if git status --porcelain | grep "M git.nix"; then 
   git add ./git.nix
   git commit -m "updates git.nix"
-  git push https://theNerd247:${GITHUB_TOKEN}@github.com/theNerd247/conix.git $branch
+  git push https://theNerd247:${GITHUB_TOKEN}@github.com/theNerd247/conix.git "$branch"
 fi
 
 ./scripts/mkDocs.sh 
@@ -27,5 +29,5 @@ fi
 if [[ $(git status --porcelain | grep "M docs.md") || $(git status --porcelain | grep "M readme.md") ]]; then 
   git add ./docs.md ./readme.md
   git commit --allow-empty -m "updates docs and readme"
-  git push https://theNerd247:${GITHUB_TOKEN}@github.com/theNerd247/conix.git $branch
+  git push https://theNerd247:${GITHUB_TOKEN}@github.com/theNerd247/conix.git "$branch"
 fi
