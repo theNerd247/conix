7318f6b909fa62f40e7b1134acba27dec666bfb3
theNerd247 <noah.harvey247 gm>
2020-08-20 14:48:25 -0400

    moves generation commits to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
 
-./scripts/mkDocs.sh \
-  && ./scripts/mkGitNix.sh
+./scripts/mkGitNix.sh
+
+git config user.email "travis@travis.org"
+git config user.name "travis"
+git add ./git.nix
+git commit --allow-empty -m "updates git.nix"
+git push https://theNerd247:${GITHUB_TOKEN}@github.com/theNerd247/conix.git HEAD:HEAD
+
+./scripts/mkDocs.sh 
+
+git config user.email "travis@travis.org"
+git config user.name "travis"
+git add ./docs.md ./readme.md
+git commit --allow-empty -m "updates docs and readme"
+git push https://theNerd247:${GITHUB_TOKEN}@github.com/theNerd247/conix.git HEAD:HEAD
