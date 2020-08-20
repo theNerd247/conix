cb280108788215e8c5a0ba6d089b962570cb03f0
theNerd247 <noah.harvey247 gm>
2020-08-20 17:48:56 -0400

    attempting hand cranked branch checkout to avoid merge commits

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,31 @@
 #!/usr/bin/env bash
 
-./scripts/mkGitNix.sh
+# Pull down the branch originating the PR or branch pushed to
+# This makes it so we can create fixup commits
+if $TRAVIS_PULL_REQUEST; then
+  branch=$TRAVIS_PULL_REQUEST_BRANCH
+else
+  branch=$TRAVIS_BRANCH
+fi
 
+git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+git fetch $branch
+git checkout -t origin/branch
 git config user.email "travis@travis.org"
 git config user.name "travis"
-git add ./git.nix
-git commit --allow-empty -m "updates git.nix"
-git push https://theNerd247:${GITHUB_TOKEN}@github.com/theNerd247/conix.git HEAD:HEAD
+
+./scripts/mkGitNix.sh
+
+if git status --porcelain | grep "M git.nix"; then 
+  git add ./git.nix
+  git commit -m "updates git.nix"
+  git push https://theNerd247:${GITHUB_TOKEN}@github.com/theNerd247/conix.git $branch
+fi
 
 ./scripts/mkDocs.sh 
 
-git config user.email "travis@travis.org"
-git config user.name "travis"
-git add ./docs.md ./readme.md
-git commit --allow-empty -m "updates docs and readme"
-git push https://theNerd247:${GITHUB_TOKEN}@github.com/theNerd247/conix.git HEAD:HEAD
+if [[ $(git status --porcelain | grep "M docs.md") || $(git status --porcelain | grep "M readme.md") ]]; then 
+  git add ./docs.md ./readme.md
+  git commit --allow-empty -m "updates docs and readme"
+  git push https://theNerd247:${GITHUB_TOKEN}@github.com/theNerd247/conix.git $branch
+fi
