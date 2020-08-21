e492eda648d60677d286f971eb80ed8c7b1ee18a
theNerd247 <noah.harvey247 gm>
2020-08-21 19:00:21 -0400

    switches ref and rev

--- git.nix
+++ git.nix
@@ -1,17 +1,22 @@
 let
   refDir = ./.git/refs/heads;
   refFile = ./.git/HEAD;
+  refName = builtins.replaceStrings ["ref: refs/heads/" "\n"] ["" ""]  (builtins.readFile refFile);
+
+  # If the ref is 40 characters long then it's most likely a hash
+  # in which case we're in a detached head state and are unable to point
+  # to a ref - so we'll do without...
+  isHeadless = builtins.stringLength refName == 40; 
+  rev = if isHeadless 
+    then refName
+    else
+      builtins.replaceStrings ["\n"] [""] 
+        (builtins.readFile "${refDir}/${refName}");
+
+  ref = if isHeadless then "HEAD" else refName;
 in
 rec
 {
   url = "https://github.com/theNerd247/conix.git"; 
-  ref = builtins.replaceStrings ["ref: refs/heads/" "\n"] ["" ""]  (builtins.readFile refFile);
-  rev = 
-    # If the ref is 40 characters long then it's most likely a hash
-    # in which case we're in a detached head state and are unable to point
-    # to a ref - so we'll do without...
-    if builtins.stringLength ref == 40 then ""
-    else
-      builtins.replaceStrings ["\n"] [""] 
-        (builtins.readFile "${refDir}/${ref}");
+  inherit rev ref;
 }
