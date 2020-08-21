7f62186d4f3da6f85341f715ec73dceed3958cbb
theNerd247 <noah.harvey247 gm>
2020-08-20 22:48:13 -0400

    enables pre-commit

--- git.nix
+++ git.nix
@@ -2,8 +2,8 @@ conix: { lib = rec
   { git =
     rec
     { url = "https://github.com/theNerd247/conix.git"; 
-      rev = "84d98837829587ce3ba2761f5d76e5d9da0c34d9";
-      ref = "v0.1.0-api";
+      rev = "577d1c8cc6efbeb89c912a74fc2c8d8ac36592e2";
+      ref = "travis";
       text = ''
         { url = "${url}";
           rev = "${rev}";
