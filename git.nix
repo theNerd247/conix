458b92a50c2c4464069974768dc79c7ed89900a4
theNerd247 <noah.harvey247 gm>
2020-08-21 18:08:28 -0400

    replaces bash script to generate git info with pure nix code

--- git.nix
+++ git.nix
@@ -1,15 +1,8 @@
-conix: { lib = rec
-  { git =
-    rec
-    { url = "https://github.com/theNerd247/conix.git"; 
-      rev = "1f8a7e9e96b4709bd08c870be8ff27023500be31";
-      ref = "travis";
-      text = ''
-        { url = "${url}";
-          rev = "${rev}";
-          #ref = "${ref}";
-        }
-        '';
-    };
-  };
+rec
+{
+  refDir = ./.git/refs/heads;
+  refFile = ./.git/HEAD;
+  url = "https://github.com/theNerd247/conix.git"; 
+  ref = builtins.replaceStrings ["ref: refs/heads/" "\n"] ["" ""]  (builtins.readFile refFile);
+  rev = builtins.replaceStrings ["\n"] [""] (builtins.readFile "${refDir}/${ref}");
 }
