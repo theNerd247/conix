f0519855389041405f269d6dea4d9a1000c1fe06
theNerd247 <noah.harvey247 gm>
2020-08-21 18:21:16 -0400

    adds case where we're in a detached head state

--- git.nix
+++ git.nix
@@ -1,8 +1,17 @@
-rec
-{
+let
   refDir = ./.git/refs/heads;
   refFile = ./.git/HEAD;
+in
+rec
+{
   url = "https://github.com/theNerd247/conix.git"; 
   ref = builtins.replaceStrings ["ref: refs/heads/" "\n"] ["" ""]  (builtins.readFile refFile);
-  rev = builtins.replaceStrings ["\n"] [""] (builtins.readFile "${refDir}/${ref}");
+  rev = 
+    # If the ref is 40 characters long then it's most likely a hash
+    # in which case we're in a detached head state and are unable to point
+    # to a ref - so we'll do without...
+    if builtins.stringLength ref == 40 then ""
+    else
+      builtins.replaceStrings ["\n"] [""] 
+        (builtins.readFile "${refDir}/${ref}");
 }
