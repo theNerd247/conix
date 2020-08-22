57411216b4c1acf248b09ad7616dded5f3206020
theNerd247 <noah.harvey247 gm>
2020-08-21 20:49:41 -0400

    fixes bug with wrong branch checkout on travis

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,14 +4,15 @@ set -ex
 
 # Pull down the branch originating the PR or branch pushed to
 # This makes it so we can create fixup commits
-if [[ -n "$TRAVIS_PULL_REQUEST" ]]; then
-  branch=$TRAVIS_PULL_REQUEST_BRANCH
-else
+if [[ "$TRAVIS_PULL_REQUEST" -eq "false" ]]; then
+  echo "using travis branch"
   branch=$TRAVIS_BRANCH
+else
+  echo "using travis PR branch";
+  branch="pull/$TRAVIS_PULL_REQUEST/merge"
 fi
 
-git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
-git fetch origin "$branch"
+exit 0
 
 git checkout $branch
 
