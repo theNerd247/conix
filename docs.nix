60972491670dcedfdf3e6b50e6ee65637ff69b87
theNerd247 <noah.harvey247 gm>
2020-08-25 18:33:59 -0400

    fixes bugs from merge

--- docs.nix
+++ docs.nix
@@ -45,7 +45,7 @@ conix: { lib = rec
   mkDocs = docsAttrSet:
       collectDocModules (builtins.removeAttrs docsAttrSet ["text" "drv"]);
 
-  conixReferenceDocumentation = using [(markdownFile "docs") (htmlFile "docs" "--metadata title=docs")] (texts [
+  conixReferenceDocumentation = with conix.lib; using [(markdownFile "docs") (htmlFile "docs" "--metadata title=docs")] (texts [
       ''
       # Reference Documentation - ${conix.lib.version.text}
 
@@ -69,5 +69,5 @@ conix: { lib = rec
       ---
       Built using ${conix.lib.homePageLink} version ${conix.lib.version.text}
       ''
-    ];
+    ]);
 };}
--- documentation.nix
+++ documentation.nix
@@ -1,5 +1,6 @@
 (import <nixpkgs> { overlays = (import ./default.nix); }).conix.buildPages [
   (conix: { drv = with conix.lib;
+    let
       c = collect "conix-docs" (
         conixReferenceDocumentation
         ++ docs.readme.drvs
