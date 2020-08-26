ad89c37cb14c97002d5bebbd8c8eaba38fda7f8f
theNerd247 <noah.harvey247 gm>
2020-08-26 11:51:04 -0400

    refactors using_

--- drvs.nix
+++ drvs.nix
@@ -1,19 +1,32 @@
 conix: { lib = rec
 {
   
-  docs.using.docstr = ''
-    Constructs a derivation from the given module, and then appends
+  docs.using_.docstr = ''
+    Constructs derivations from the given module, and then append
     that to the module's derivations. 
   '';
-  docs.using.type = "[(Module -> Derivation)] -> Module -> Module";
-  using = fs: module: conix.lib.mergeModules module ({ drvs = builtins.map (f: f module) fs; });
+  docs.using_.type = "(Module -> [Derivation]) -> Module -> Module";
+  using_ = f: module: conix.lib.mergeModules module ({ drvs = f module; });
 
-  docs.as.docstr = ''
-    Like `using` but instead of appending the generated derivation, it sets the
-    drvs to the generated derivation.
+  docs.as_.docstr = ''
+    Construct derivations from the given module and then replace that modules
+    derivations with the constructed ones.
   '';
-  docs.as.type = "[(Module -> Derivation)] -> Module -> Module";
-  as = fs: module: conix.lib.setDrvs (builtins.map (f: f module) fs) module;
+  docs.as_.type = "(Module -> [Derivation]) -> Module -> Module";
+  as_ = f: module: conix.lib.setDrvs (f module) module;
+
+  docs.using.docstr = ''
+    Like `using_` but uses a list of functions that return a single derivation 
+    each.
+
+    It's more likely you'll use this instead of `using_`.
+  '';
+  docs.using.type = "[(Module -> Derivation)] -> Module -> Module";
+  using = fs: using_ (a: builtins.map (f: f a) fs) 
+
+  docs.as.docstr = builtins.replaceStrings ["using_"] ["as_"] docs.using.docstr;
+  docs.as.type = docs.using.type;
+  as = fs: as_ (a: builtins.map (f: f a) fs) 
 
   docs.dirWithMarkdown.docstr = ''
     Create a directory called `name` and within it a markdown file called
