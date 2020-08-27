1ab823819f5be9701bc377021ae0f7117f5a03ad
theNerd247 <noah.harvey247 gm>
2020-08-27 13:59:26 -0400

    makes markdown list recieve modules

--- markdown.nix
+++ markdown.nix
@@ -3,11 +3,13 @@ conix: { lib = rec
     docs.md.list.docstr = ''
       Create an bullet list style markdown list.
       '';
-    docs.md.list.type = "Name -> [ String ] -> Module";
+    docs.md.list.type = "Name -> [ Module ] -> Module";
     md.list
-      = name: texts: 
-        { text = builtins.concatStringsSep "\n" (builtins.map (t: "* ${t}") texts);
-          ${name} = texts;
-        };
+      = name: ms:
+        let
+          modules = builtins.map conix.lib.toTextModule ms;
+          text = builtins.concatStringsSep "\n" (builtins.map (m: "* ${m.text}") modules);
+        in
+          conix.lib.mergeModules ((conix.lib.foldModules modules) // { inherit text; }) { ${name} = ms; };
   };
 }
