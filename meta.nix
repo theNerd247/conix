8433338d08704b39ce568c70b8a71b81c756bb93
theNerd247 <noah.harvey247 gm>
2020-08-28 14:37:29 -0400

    adds alt text to status badge

--- meta.nix
+++ meta.nix
@@ -2,7 +2,7 @@ conix: { lib = rec
   { 
     homepageUrl = "https://theNerd247.github.io/conix";
     homePageLink = "<a href=\"${homepageUrl}\">conix</a>";
-    buildBadgeLink = "![](https://travis-ci.com/theNerd247/conix.svg?branch=${conix.lib.git.ref})";
+    buildBadgeLink = "![Travis Build Status](https://travis-ci.com/theNerd247/conix.svg?branch=${conix.lib.git.ref})";
     version = rec  
       { text = "${builtins.toString major}.${builtins.toString minor}.${builtins.toString patch}";
         major = 0; 
