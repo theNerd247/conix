889d5f4d0e03b90e303d0d3cf119204c615f7cda
theNerd247 <noah.harvey247 gm>
2020-08-27 22:38:00 -0400

    changes build system to place docs in their own directory

--- documentation.nix
+++ documentation.nix
@@ -1,11 +1,13 @@
 (import <nixpkgs> { overlays = (import ./default.nix); }).conix.buildPages [
   (conix: { drv = with conix.lib;
     let
-      c = collect "conix-docs" (
-        conixReferenceDocumentation.drvs
-        ++ docs.readme.drvs
-        ++ docs.goals.drvs
-        ++ docs.integration.drvs
+      c = dir "conix-docs" (
+           docs.readme.drvs
+        ++ [ (dir "docs" (
+          conixReferenceDocumentation.drvs
+          ++ docs.goals.drvs
+          ++ docs.integration.drvs
+        ))] 
       );
     in
       { drvs = [ c ]; };
--- scripts/mkDocs.sh
+++ scripts/mkDocs.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 docs=$(nix-build ./documentation.nix --show-trace)
-cp --no-preserve=mode $docs/*.md ./.
+cp --no-preserve=mode $docs/readme.md ./readme.md
+cp --no-preserve=mode -r $docs/docs ./docs
