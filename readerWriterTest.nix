bdab0402219fe648c535b477039c274dc9474337
theNerd247 <noah.harvey247 gm>
2020-09-08 21:23:00 -0400

    fixes imports for readerWriterText

--- readerWriterTest.nix
+++ readerWriterTest.nix
@@ -1,12 +1,12 @@
 let
   pkgs = import <nixpkgs> {};
   types = (import ./types.nix) pkgs;
-  rw = (import ./state.nix) types;
+  rw = (import ./readerWriter.nix) types;
 
   eval = types.cata (types.fmapFree rw.fmap) (types.match
     { "tell" = {tell, _next}: pkgs.lib.attrsets.recursiveUpdate _next tell;
       "ask"  = runGet: runGet { x = 2; };
-      "pure" = x: { pure = x; };
+      "pure" = _: {};
     }
   );
 
@@ -14,6 +14,6 @@ let
 
 in 
   { 
-    askTell = with rw; run { x = 2; pure = null;} 
+    askTell = with rw; run { x = 2; } 
       (eval (ask ({x}: tell { tell = { inherit x; }; _next = types.pure null; })));
   }
