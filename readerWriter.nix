8e9a139706b4fa403d4b10fc913cbd7c4fecdf39
theNerd247 <noah.harvey247 gm>
2020-09-08 22:19:58 -0400

    adds collectData to readerWriter.nix

--- readerWriter.nix
+++ readerWriter.nix
@@ -2,20 +2,27 @@
 
 rec
 {
-  docs.set.type = " { _entry :: AttrSet; _next :: a } -> StateF a }";
+  docs.readerWriter.set.type = " { _entry :: AttrSet; _next :: a } -> RWF a";
   tell = typed "tell";
 
-  docs.get.type = "(AttrSet -> a) -> StateF a";
+  docs.readerWriter.get.type = "(AttrSet -> a) -> RWF a";
   ask = f: 
     if ! builtins.isFunction f 
     then throw "Invalid type: argument to get must be a function"
     else typed "ask" f;
 
-  docs.pure.type = "a -> StateF a";
+  docs.pure.type = "a -> RWF a";
   pure = x: ask (_: x);
 
+  docs.readerWriter.fmap = "(a -> b) -> RWF a -> RWF b";
   fmap = f: match
     { "tell"  = x: tell { inherit (x) _entry; _next = (f x._next); };
       "ask"   = g: ask (x: f (g x));
+    };
+
+  docs.readerWriter.collectData.type = "RWF (AttrSet -> AttrSet) -> (AttrSet -> AttrSet)";
+  collectData = types.match
+    { "ask"  = f: x: f x x;
+      "tell" = {_entry, _next}: x: pkgs.lib.attrsets.recursiveUpdate (_next x) _entry;
     };
 }
