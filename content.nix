fb4013f39a3b7ac9024214e1b34ac9c5da603ca4
theNerd247 <noah.harvey247 gm>
2020-09-10 15:19:52 -0400

    fixes infinite recursion bug

--- content.nix
+++ content.nix
@@ -4,13 +4,24 @@ rec
 {  
   fmapMatch = f: (RW.fmapMatch f) // (M.fmapMatch f);
 
+  # 
+  # x :: a -> Fix F
+  # cata :: Fix F -> a
+  # 
+  # eval :: (a -> Fix F) -> a -> a
+  #
+  #
+  # Fix f -> (a -> a)
+  #
+  #
+
   docs.contentWriter.eval.type = ''
     (AttrSet -> { data :: AttrSet, text :: String } ~ t)
     => FreeF b ContentF t -> t
   '';
   eval = types.match
     { "pure" = _: __: { data = {}; text = ""; };
-      "text" = t: a: { data = a; text = t; };
+      "text" = t: _: { data = {}; text = t; };
       "ask"  = f: x: f x x;
       "tell" = {_entry, _next}: x: 
         let
