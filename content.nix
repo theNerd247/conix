7208473fbcf824d04745b4646c27bf0befd3b18f
theNerd247 <noah.harvey247 gm>
2020-09-10 16:50:19 -0400

    refactors eval

--- content.nix
+++ content.nix
@@ -2,22 +2,41 @@ pkgs: types: RW: M:
 
 rec
 {  
+  # ContentF = RWF + MarkupF
   fmapMatch = f: (RW.fmapMatch f) // (M.fmapMatch f);
 
-  docs.contentWriter.eval.type = ''
+  mempty = _: { data = {}; text = ""; };
+
+  memptyWithText = text: _: { data = {}; inherit text; };
+  memptyWithData = data: _: { inherit data; text = ""; };
+
+  mappend = f: g: x:
+    let
+      r1 = f x;
+      r2 = g x;
+    in
+      { data = pkgs.lib.attrsets.recursiveUpdate r1.data r2.data;
+        text = r1.text + r2.text;
+      };
+
+  mconcat = builtins.foldl' mappend mempty;
+
+  # join :: (a -> (a -> b)) -> a -> b
+  join = f: x: f x x;
+
+  docs.content.eval.type = ''
     (AttrSet -> { data :: AttrSet, text :: String } ~ t)
     => FreeF b ContentF t -> t
   '';
   eval = types.match
-    { "pure" = _: __: { data = {}; text = ""; };
-      "text" = t: _: { data = {}; text = t; };
-      "ask"  = f: x: f x x;
-      "tell" = {_entry, _next}: x: 
-        let
-          res = _next x;
-        in
-          { data = pkgs.lib.attrsets.recursiveUpdate res.data _entry;
-            inherit (res) text;
-          };
+    { 
+      "pure" = _: mempty;
+      "text" = memptyWithText;
+      "doc"  = mconcat;
+      "ask"  = join;
+      # NOTE: if we used the Freer encoding then this could be simplified
+      # to memptyWithData and the interpretation of bind would take care
+      # of the mappend
+      "tell" = {_entry, _next}: mappend _next (memptyWithData _entry);
     };
 }
