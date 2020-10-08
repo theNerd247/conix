34d403b6e6388cb50e51e92441b180c3ec8a50ca
theNerd247 <noah.harvey247 gm>
2020-10-08 14:34:26 -0400

    adds printNixValue.nix

---
+++ printNixValue.nix
@@ -0,0 +1,35 @@
+let
+
+  printAttrs = e:
+    let
+      printElem = name: value:
+        "${name} = ${ printNixVal value };";
+
+      printElems = 
+        builtins.concatStringsSep " "
+          (conix.pkgs.lib.attrsets.mapAttrsToList printElem e);
+    in
+      "{ ${printElems} }";
+
+  printList = e:
+    let
+      printElems = builtins.concatStringsSep " "
+        (builtins.map printNixVal e);
+    in
+      "[ ${printElems} ]";
+in
+{
+  docs.printNixVal.docstr = ''
+    Pretty print a pure nix-value. 
+
+    NOTE: do not call this function on a derivation as it will segfault.
+    '';
+  docs.printNixVal.type = "a -> String";
+  printNixVal = e:
+    if builtins.isAttrs e then printAttrs e
+    else if builtins.isList e then printList e
+    else if builtins.isNull e then "null"
+    else if builtins.isFunction e then "<lambda>"
+    else builtins.toString e;
+
+}
