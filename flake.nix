70be455f390db415a672e8dbb913f089558f239b
Noah Harvey <noah.harvey247@gmail.com>
2025-03-06 13:54:37 -0500

    allows multiple architectures

--- flake.nix
+++ flake.nix
@@ -4,9 +4,12 @@
   };
   
   outputs = inputs: 
-    let
-      pkgs = import inputs.nixpkgs { system = "aarch64-darwin"; };
-    in
-      { conix = (import ./lib/default.nix pkgs);
+    { packages.aarch64-darwin = 
+      { conix = (import ./lib/default.nix (import inputs.nixpkgs { system = "aarch64-darwin"; }));
       };
+
+      packages.x86_64-linux = 
+      { conix = (import ./lib/default.nix (import inputs.nixpkgs { system = "x86_64-linux"; }));
+      };
+    };
 }
